2b3b588f49c79264febdf4fc55eab7cb3f0a0a79
rezabarzakhi <reza.brzkhi@gmail.com>
2023-06-13 16:10:15 +0330

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,13 @@
 import Home from "./Pages/Home/Home";
 import Menu from "./Pages/Menu/Menu";
-import Branches from "./Pages/branches/Branches";
+import Branches from "./Pages/Branches/Branches";
 let routes = [
-    { path: "/", element: <Home /> },
-    { path: "/menu", element: <Menu /> },
-    { path: "/branches/:ID", element: <Branches /> },
-    // { path: "/", element: <Home /> },
-    // { path: "/", element: <Home /> },
-    // { path: "/", element: <Home /> },
-
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/branches/:ID", element: <Branches /> },
+  // { path: "/", element: <Home /> },
+  // { path: "/", element: <Home /> },
+  // { path: "/", element: <Home /> },
 ];
 
-export default routes
+export default routes;
